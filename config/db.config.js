b4b22f7fd33177de50a6318382bc91d0327b87c6
Rodrigo Matos <rodrigomatosrj@gmail.com>
2020-12-10 23:07:01 -0300

    signup done

---
+++ config/db.config.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+async function connect() {
+	try {
+		const connection = await mongoose.connect(process.env.MONGODB_URI, {
+			useCreateIndex: true,
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useFindAndModify: false,
+		});
+		console.log("Connected to DB: ", connection.connection.name);
+	} catch (err) {
+		console.error("Database connection error: ", err);
+	}
+}
+
+module.exports = connect;
