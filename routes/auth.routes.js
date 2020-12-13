0f38eb53d923872b8bcce9f1658086d530bdb4d9
Rodrigo Matos <rodrigomatosrj@gmail.com>
2020-12-13 07:48:22 -0300

    google fix v2

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const passport = require("passport");
 
-router.get("/auth/google", (req, res, next) => {
+router.get("/google", (req, res, next) => {
 	passport.authenticate("google", {
 		scope: [
 			"https://www.googleapis.com/auth/userinfo.profile",
@@ -10,9 +10,9 @@ router.get("/auth/google", (req, res, next) => {
 	})(req, res, next);
 });
 
-router.get("/auth/google/callback", (req, res, next) => {
+router.get("/google/callback", (req, res, next) => {
 	passport.authenticate("google", {
-		successRedirect: "/private-page",
+		successRedirect: "/dashboard",
 		failureRedirect: "/", // here you would redirect to the login page using traditional login approach
 	})(req, res, next);
 });
