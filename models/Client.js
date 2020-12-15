2f25841f756d8cf93fe83d34f84a5cf5bb450860
lauraferraz5 <lauraferraz5@gmail.com>
2020-12-15 11:11:03 -0300

    add infos client

--- models/Client.js
+++ models/Client.js
@@ -5,7 +5,11 @@ const ClientSchema = new Schema({
 	mobile: { type: String, required: true },
 	adress: {
 		street: { type: String, required: true },
+		number: { type: String, required: true },
+		complement: { type: String, requires: true },
 		city: { type: String, required: true },
+		state: { type: String, required: true },
+		zipcode: { type: String, required: true },
 	},
 	pets_id: [{ type: Schema.Types.ObjectId, ref: "Pet" }],
 	user_id: { type: Schema.Types.ObjectId, ref: "User" },
