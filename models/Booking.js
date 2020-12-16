7c26c2c6d9966860b6e1fc39ac6f3949961fcfec
Rodrigo Matos <rodrigomatosrj@gmail.com>
2020-12-16 18:36:11 -0300

    booking

--- models/Booking.js
+++ models/Booking.js
@@ -3,8 +3,8 @@ const { Schema, model } = require("mongoose");
 const BookingSchema = new Schema({
 	user_id: { type: Schema.Types.ObjectId, ref: "Client" },
 	pets: [{ type: Schema.Types.ObjectId, ref: "Pet" }],
-	start_date: { type: Date, required: true },
-	end_date: { type: Date, required: true },
+	startDate: { type: Date, required: true },
+	endDate: { type: Date, required: true },
 	value: { type: Number, required: true },
 	payment: { type: Boolean, default: false },
 	accommodation_id: { type: Schema.Types.ObjectId, ref: "Accommodation" },
