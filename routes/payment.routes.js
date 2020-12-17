aacec9693738719d0e8cc46eefbd3f2e63e49004
Rodrigo Matos <rodrigomatosrj@gmail.com>
2020-12-17 13:49:44 -0300

    pay

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -6,17 +6,21 @@ const Booking = require("../models/Booking");
 router.post(
 	"/",
 	passport.authenticate("jwt", { session: false }),
+	// setTimeout(() => {
+
+	// );
+
 	async (req, res) => {
 		try {
-			
-			setTimeout(() => {
-				await Payment.create({ user_id: req.user._id }, req.body);
-				await Booking.findOneAndUpdate(
+			for (let i = 1; i <= 12000; i++) {
+				continue;
+			}
+			await Payment.create({ user_id: req.user._id }, req.body);
+			await Booking.findOneAndUpdate(
 				{ _id: req.body.booking_id },
 				{ payment: true }
 			);
-				res.status(200).json({ message: "Payment received sucessfully!!!" });
-			}, 6000);
+			res.status(200).json({ message: "Payment received sucessfully!!!" });
 		} catch (err) {
 			console.error(err);
 			return res.status(500).json({ msg: err });
