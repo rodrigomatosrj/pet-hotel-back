d6d02d54859306ded698d4661a9416bffb0fa452
Rodrigo Matos <rodrigomatosrj@gmail.com>
2020-12-17 14:21:40 -0300

    timeout

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -6,21 +6,22 @@ const Booking = require("../models/Booking");
 router.post(
 	"/",
 	passport.authenticate("jwt", { session: false }),
-	// setTimeout(() => {
-
-	// );
-
 	async (req, res) => {
 		try {
 			for (let i = 1; i <= 12000; i++) {
 				continue;
 			}
-			await Payment.create({ user_id: req.user._id }, req.body);
+			await Payment.create(
+				{ user_id: req.user._id },
+				{ ...req.body, authorization_code: "@231131287#@@!@" }
+			);
 			await Booking.findOneAndUpdate(
 				{ _id: req.body.booking_id },
 				{ payment: true }
 			);
-			res.status(200).json({ message: "Payment received sucessfully!!!" });
+			setTimeout(() => {
+				res.status(200).json({ message: "Payment received sucessfully!!!" });
+			}, 5000);
 		} catch (err) {
 			console.error(err);
 			return res.status(500).json({ msg: err });
