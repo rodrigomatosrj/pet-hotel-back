67871b7d5d0c73ce3823b16ce97f86b0cdf0ea65
Rodrigo Matos <rodrigomatosrj@gmail.com>
2020-12-17 10:02:10 -0300

    payment model

--- models/Payment.js
+++ models/Payment.js
@@ -1,5 +1,7 @@
 const { Schema, model } = require("mongoose");
 
+
+
 const UserSchema = new Schema({
 	booking_id: { type: Schema.Types.ObjectId, ref: "Booking" },
 	user_id: { type: Schema.Types.ObjectId, ref: "User" },
